d9c5658798cf62e9d87a1d2fd46f12cf351fdf7c
JulianDbs <jdbsildesmode@gmail.com>
2022-06-02 19:42:31 -0300

    First Commit!!! Extracted files from ZIP to project main directory.

---
+++ src/main/java/com/juliandbs/savemynotes/SavemynotesApplication.java
@@ -0,0 +1,13 @@
+package com.juliandbs.savemynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavemynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavemynotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juliandbs/savemynotes/SavemynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliandbs.savemynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavemynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
