de229e712bf5bb90cf7e8b3fe38d2bfb54c3a389
JulianDbs <jdbsildesmode@gmail.com>
2022-06-05 12:03:15 -0300

    Added missing String property value in 'setEmail()' method. Added 'final' keywodk to 'setUsername()' method property. Added 'final' keywodk to 'setPassword()' method property. Added 'final' keywodk to 'setMatchingPassword()' method property. Deleted '@NotEmpty' annotation in 'username' class property. Deleted '@NotNull' annotation in 'email' class property. Deleted '@NotEmpty' annotation in 'email' class property. Deleted '@Size' annotation in 'emial' class property. Added '@ValidEmail' annotation in 'email' class property. Deleted '@Size' annotation in 'matchingPassword' class property. Added '@PasswordMatches' annotation to class declaration. imported '@ValidEmail' and '@PasswordMatches' annotations.

--- src/main/java/com/juliandbs/savemynotes/registration/dto/UserDto.java
+++ src/main/java/com/juliandbs/savemynotes/registration/dto/UserDto.java
@@ -2,6 +2,8 @@ package com.juliandbs.savemynotes.registration.dto;
 
 import com.juliandbs.savemynotes.registration.annotations.ValidUsername;
 import com.juliandbs.savemynotes.registration.annotations.ValidUsernameCharacters;
+import com.juliandbs.savemynotes.registration.annotations.ValidEmail;
+import com.juliandbs.savemynotes.registration.annotations.PasswordMatches;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotEmpty;
@@ -10,17 +12,15 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
+@PasswordMatches()
 public class UserDto {
 
 	@ValidUsername
 	@ValidUsernameCharacters
-	@NotEmpty(message="Username field is empty")
 	@Size(message="Username length must be greater than 4 and less than 20", min=4, max=21)
 	private String username;
 
-	@NotNull
-	@NotEmpty(message="Email field is empty")
-	@Size(message="Email length must be greater than 4 and less than 20", min=5, max=21)
+	@ValidEmail
 	private String email;
 
 	@NotNull
@@ -29,26 +29,26 @@ public class UserDto {
 	private String password;
 
 	@NotNull
-	@Size(message="Email length must be greater than 4 and less than 20", min=5, max=21)
-	@NotEmpty(message="Username field is empty")
+	@Size(message="Password length must be greater than 4 and less than 20", min=5, max=21)
+	@NotEmpty(message="Password field is empty")
 
 	private String matchingPassword;
 
 	public String getUsername() {return username;}
 
-	public void setUsername(String username) {this.username = username;}
+	public void setUsername(final String username) {this.username = username;}
 
 	public String getEmail() {return email;}
 
-	public void setEmail() {this.email = email;}
+	public void setEmail(final String email) {this.email = email;}
 
 	public String getPassword() {return password;}
 
-	public void setPassword(String password) {this.password = password;}
+	public void setPassword(final String password) {this.password = password;}
 
 	public String getMatchingPassword() {return matchingPassword;}
 
-	public void setMatchingPassword(String matchingPassword) {this.matchingPassword = matchingPassword;}
+	public void setMatchingPassword(final String matchingPassword) {this.matchingPassword = matchingPassword;}
 
 	@Override
 	public String toString() {
