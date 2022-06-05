b5bedc861955ac99c25edfbaf1243d1ff004b375
JulianDbs <jdbsildesmode@gmail.com>
2022-06-05 12:18:18 -0300

    Imported 'ValidationErrorFilter' component. Deleted 'BindingResult' class import line. Imported 'Autowired' annotation. Deleted 'HttpServletRequest' class import line. Added 'ValidationErrorFilter' comonent to class. Added '@Autowired' annotation to 'ValidationErrorFilter' component. Separated 'UserDto' initialization from 'model.addAttribute()' method call. Rewritten 'registerNewUser()' method.

--- src/main/java/com/juliandbs/savemynotes/registration/controllers/RegistrationController.java
+++ src/main/java/com/juliandbs/savemynotes/registration/controllers/RegistrationController.java
@@ -1,40 +1,45 @@
 package com.juliandbs.savemynotes.registration.controllers;
 
 import com.juliandbs.savemynotes.registration.dto.UserDto;
+import com.juliandbs.savemynotes.main.utils.ValidationErrorFilter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.beans.factory.annotation.Autowired;
 import javax.validation.Valid;
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RegistrationController {
 
+	@Autowired
+	private ValidationErrorFilter validationErrorFilter;
+
 	@GetMapping("/registration")
         public String getRegistration(Model model) {
-                model.addAttribute("registryForm", new UserDto() );
+		UserDto user = new UserDto();
+                model.addAttribute("user", user);
                 return "registry/registry";
         }
 
 	@PostMapping("/registration")
 	public String registerUserAccount(
-			@ModelAttribute("registryForm")
-			@Valid final UserDto userDto,
-			final BindingResult bindingResult,
-			final HttpServletRequest request,
-			final Errors errors
-	) {
-		System.out.println("Acount : (" + userDto.toString() + ")");
-		System.out.println("Errors count : (" + errors.getErrorCount() + ")");
-
+			@Valid
+			@ModelAttribute("user")
+			UserDto user,
+			Errors errors,
+			Model model
+	){
+		System.out.println("Acount : (" + user.toString() + ")");
 		String toUrl = "login/login";
 		if (errors.hasErrors()) {
 			toUrl = "registry/registry";
+			model.addAttribute("usernameErrors", validationErrorFilter.getUsernameErrors(errors));
+			model.addAttribute("emailErrors", validationErrorFilter.getEmailErrors(errors));
+			model.addAttribute("passwordErrors", validationErrorFilter.getPasswordErrors(errors));
 		}
 		return toUrl;
 	}
