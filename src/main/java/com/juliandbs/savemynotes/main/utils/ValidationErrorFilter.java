73d47583424909498008d01955edb5fff9dd91a5
JulianDbs <jdbsildesmode@gmail.com>
2022-06-05 12:33:46 -0300

    Added 'utils' folder in 'src/main/java/com/juliandbs/savemynotes/main/' directory. Added 'ValidationErrorFilter.java' file in 'utils' folder.

---
+++ src/main/java/com/juliandbs/savemynotes/main/utils/ValidationErrorFilter.java
@@ -0,0 +1,61 @@
+package com.juliandbs.savemynotes.main.utils;
+
+import com.juliandbs.savemynotes.registration.annotations.ValidUsername;
+import com.juliandbs.savemynotes.registration.annotations.ValidUsernameCharacters;
+import com.juliandbs.savemynotes.registration.annotations.PasswordMatches;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+import java.lang.NullPointerException;
+
+@Component("validationErrorFilter")
+public class ValidationErrorFilter {
+	private static List<String> usernameClassList = new LinkedList<>();
+	private static List<String> emailClassList = new LinkedList<>();
+	private static List<String> passwordClassList = new LinkedList<>();
+	static {
+		usernameClassList.add(ValidUsername.class.getSimpleName());
+		usernameClassList.add(ValidUsernameCharacters.class.getSimpleName());
+		passwordClassList.add(PasswordMatches.class.getSimpleName());
+	}
+
+	public List<String> getUsernameErrors(Errors errors) throws NullPointerException {
+		if (errors == null)
+			throw new NullPointerException();
+		return filterList(errors, usernameClassList);
+	}
+
+	public List<String> getEmailErrors(Errors errors) throws NullPointerException {
+		if (errors == null)
+			throw new NullPointerException();
+		return filterList(errors, emailClassList);
+	}
+
+	public List<String> getPasswordErrors(Errors errors) throws NullPointerException {
+		if (errors == null)
+			throw new NullPointerException();
+		return filterList(errors, passwordClassList);
+	}
+
+	private List<String> filterList(Errors errors, List<String> classList) {
+		List<ObjectError> errorList = errors.getAllErrors();
+		List<ObjectError> finalList = new LinkedList<>();
+		for (String usernameClass : classList) {
+			finalList.addAll(
+				errorList.stream()
+						.filter(e -> e.getCode().equals(usernameClass))
+						.collect(Collectors.toCollection(LinkedList::new))
+			);
+		}
+		List<String> messages = finalList.stream()
+							.map( e -> e.getDefaultMessage())
+							.collect(Collectors.toCollection(LinkedList::new));
+		return messages;
+	}
+}
