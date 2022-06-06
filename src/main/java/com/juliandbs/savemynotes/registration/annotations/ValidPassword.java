46ea45a781941618979c914dea0af1eac79a28bb
JulianDbs <jdbsildesmode@gmail.com>
2022-06-06 07:30:34 -0300

    Added 'ValidPassword.java' file in 'src/main/java/com/juliandbs/savemynotes/registration/annotations/' directory.

---
+++ src/main/java/com/juliandbs/savemynotes/registration/annotations/ValidPassword.java
@@ -0,0 +1,28 @@
+package com.juliandbs.savemynotes.registration.annotations;
+
+import com.juliandbs.savemynotes.registration.validations.PasswordValidator;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Constraint(validatedBy = PasswordValidator.class)
+@Retention(RUNTIME)
+public @interface ValidPassword {
+
+	String message() default "Null or Empty Email";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
