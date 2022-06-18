91e7164475be3841edf3bfd056ef3fe89d120427
JulianDbs <jdbsildesmode@gmail.com>
2022-06-18 10:52:12 -0300

    Deleted commented lines in 'src/main/java/com/juliandbs/savemynotes/registration/controllers/RegistrationController.java' file.

--- src/main/java/com/juliandbs/savemynotes/registration/controllers/RegistrationController.java
+++ src/main/java/com/juliandbs/savemynotes/registration/controllers/RegistrationController.java
@@ -2,7 +2,6 @@ package com.juliandbs.savemynotes.registration.controllers;
 
 import com.juliandbs.savemynotes.persistence.services.UserServiceImpl;
 import com.juliandbs.savemynotes.registration.dto.UserDto;
-//import com.juliandbs.savemynotes.main.utils.ValidationErrorFilter;
 import com.juliandbs.savemynotes.main.utils.CustomResponse;
 
 import org.springframework.stereotype.Controller;
@@ -16,9 +15,6 @@ import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
-
-	//@Autowired
-	//private ValidationErrorFilter validationErrorFilter;
 
 	@Autowired
 	UserServiceImpl userService;
@@ -38,20 +34,6 @@ public class RegistrationController {
 			Errors errors,
 			Model model
 	){
-		System.out.println("Acount : (" + user.toString() + ")");
-		/*
-		String toUrl = "login/login";
-		if (errors.hasErrors()) {
-			toUrl = "registration/registration";
-			model.addAttribute("usernameErrors", validationErrorFilter.getUsernameErrors(errors));
-			model.addAttribute("emailErrors", validationErrorFilter.getEmailErrors(errors));
-			model.addAttribute("passwordErrors", validationErrorFilter.getPasswordErrors(errors));
-			model.addAttribute("matchingPasswordErrors", validationErrorFilter.getMatchingPasswordErrors(errors));
-		} else {
-			model.addAttribute("name", user.getUsername());
-			toUrl="registration/success_registration";
-		}
-		*/
 		CustomResponse customResponse = userService.createNewUser(model, errors, user);
 		model = customResponse.getModel();
 		return customResponse.getUrl();
