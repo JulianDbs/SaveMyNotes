bef41266f739aafd53733524075b0bc9e1b9382f
JulianDbs <jdbsildesmode@gmail.com>
2022-06-18 10:34:31 -0300

    Changed 'GenerationValue' srategy from 'SEQUENCE' to 'IDENTITY' on 'id' class property in 'src/main/java/juliandbs/savemynotes/persistence/models/User.java' file.

--- src/main/java/com/juliandbs/savemynotes/persistence/models/User.java
+++ src/main/java/com/juliandbs/savemynotes/persistence/models/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Column;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE )
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 
 	@Column(name = "username", nullable = false, length = 21)
