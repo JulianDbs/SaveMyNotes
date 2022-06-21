7a23ad3ac7eb2e45212acbeb98d78132d6fe3576
JulianDbs <jdbsildesmode@gmail.com>
2022-06-21 14:14:38 -0300

    Added 'user_info' creation table SQL query in 'SQL_SCRIPTS/0_create_tables.sql' file.

--- SQL_SCRIPTS/0_create_tables.sql
+++ SQL_SCRIPTS/0_create_tables.sql
@@ -5,3 +5,17 @@ create table users(
 	email varchar(40) unique not null,
 	password varchar(100) not null
 );
+
+create table user_info(
+	id serial not null,
+	email varchar(40) not null unique,
+	creation_date DATE DEFAULT CURRENT_DATE not null,
+	note_count NUMERIC not null,
+	account_expired BOOLEAN DEFAULT FALSE not null,
+	account_locked BOOLEAN DEFAULT FALSE not null,
+	creadentials_expired BOOLEAN DEFAULT FALSE not null,
+	account_enabled BOOLEAN DEFAULT TRUE not null,
+	constraint fk_email
+		foreign key (email)
+			references users(email)
+);
