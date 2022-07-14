3edd2215154a18aa21bec2f5a53fcf19651b90c1
JulianDbs <jdbsildesmode@gmail.com>
2022-07-14 04:15:26 -0300

    Added '/new-note' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file. Added '/note-not-found' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file. Added '/error-401' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file. Added '/error-403' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file. Added '/error-404' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file. Added '/error-500' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file. Added '/edit-note-success' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file. Added '/edit-note-failure' view registry in 'addViewControllers(ViewControllerRegistry registry)' method in 'src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java' file.

--- src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java
+++ src/main/java/com/juliandbs/savemynotes/main/configs/MvcConfig.java
@@ -13,5 +13,15 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/desktop").setViewName("desktop/desktop");
 		registry.addViewController("/login").setViewName("login/login");
 		registry.addViewController("/registration").setViewName("registration/registration");
+		registry.addViewController("/new-note").setViewName("note/new-note");
+		//delete this after
+		registry.addViewController("/note-not-found").setViewName("note/note-not-found");
+		registry.addViewController("/error-401").setViewName("errors/error-401");
+		registry.addViewController("/error-403").setViewName("errors/error-403");
+		registry.addViewController("/error-404").setViewName("errors/error-404");
+		registry.addViewController("/error-500").setViewName("errors/error-500");
+		//and delete this ??
+		registry.addViewController("/edit-note-success").setViewName("note/edit-note-success");
+		registry.addViewController("/edit-note-failure").setViewName("note/edit-note-failure");
 	}
 }
