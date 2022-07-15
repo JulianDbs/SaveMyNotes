a250d506952e7d6d7ce7d007cabab57918af6a3f
JulianDbs <jdbsildesmode@gmail.com>
2022-07-15 02:12:29 -0300

    Imported 'src/main/java/com/juliandbs/savemynotes/desktop/services/DesktopServiceImpl' class in 'src/main/java/com/juliandbs/savemynotes/desktop/controllers/DesktopController.java' file. Added 'getHomeView(Model mode, Authentication auth)' method in 'src/main/java/com/juliandbs/savemynotes/desktop/controllers/DesktopController.java' file. Added 'src/main/java/com/juliandbs/savemynotes/desktop/services/DesktopServiceImpl' service implementation in 'src/main/java/com/juliandbs/savemynotes/desktop/controllers/DesktopController.java' file.

--- src/main/java/com/juliandbs/savemynotes/desktop/controllers/DesktopController.java
+++ src/main/java/com/juliandbs/savemynotes/desktop/controllers/DesktopController.java
@@ -3,6 +3,7 @@ package com.juliandbs.savemynotes.desktop.controllers;
 import com.juliandbs.savemynotes.persistence.services.NoteServiceImpl;
 import com.juliandbs.savemynotes.persistence.models.Note;
 import com.juliandbs.savemynotes.main.utils.CustomResponse;
+import com.juliandbs.savemynotes.desktop.services.DesktopServiceImpl;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,16 @@ public class DesktopController {
 
 	@Autowired
 	private NoteServiceImpl noteService;
+
+	@Autowired
+	private DesktopServiceImpl desktopService;
+
+	@GetMapping("/")
+	public String getHomeView(Model model, Authentication auth) {
+		CustomResponse response = desktopService.getHomeView(model, auth);
+		model = response.getModel();
+		return response.getUrl();
+	}
 
 	@GetMapping("/desktop")
 	public String getDesktopView(Model model, Authentication auth) {
